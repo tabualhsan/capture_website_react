0324bc143292874b98207bacd8b330d705830062
Tamara <tabualhsan@gmail.com>
2021-07-14 11:48:18 -0700

    added css styling using styled-components in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from './components/GlobalStyle';
+
+
+
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
      <AboutUs/>
     </div>
   );
