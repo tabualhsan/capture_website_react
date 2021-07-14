cc54cee85f7acd09088ab192b388b8e1a866b0a8
Tamara <tabualhsan@gmail.com>
2021-07-14 11:48:32 -0700

    added css styling using styled-components in react

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import clock from '../img/clock.svg';
+import diaphragm from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png';
+
+
+
+const ServicesSection = () => {
+    return(
+        <div className="services" >
+            <div className="description">
+                <h2>High <span>quality</span>services</h2>
+               
+                <div className="cards">
+                    <div className="card">
+                    <div className="icon">
+                        <img src={clock}/>
+                        <h3>Efficient</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                    <div className="icon">
+                        <img alt="icon" src={teamwork}/>
+                        <h3>Teamwork</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                    <div className="icon">
+                        <img alt="icon"src={diaphragm}/>
+                        <h3>Diaphram</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                    <div className="icon">
+                        <img alt="icon" src={money}/>
+                        <h3>Affortability</h3>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                </div>
+            </div>
+            <div className="image">
+                <img alt="icon" src={home2}/>
+            </div>
+        </div>
+
+
+    )
+}
+
+
+export default ServicesSection 
